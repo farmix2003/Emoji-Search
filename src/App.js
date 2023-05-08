0aa780cc6af6c4a288128932c27a19d3e7f9c52c
Farmix2003 <ffarrux386@gmail.com>
2023-05-08 07:25:28 +0300

    Emoji Search

--- src/App.js
+++ src/App.js
@@ -1,134 +1,63 @@
 import "./App.css";
-import { useState } from "react";
-import questions from "./db.json";
-import { Box, Button, Container } from "@mui/material";
-import { ArrowLeft, ArrowRight } from "@mui/icons-material";
-function App() {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [score, setScore] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
+import { useEffect, useState } from "react";
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    fetch(
+      "https://emoji-api.com/emojis?access_key=67aa650ef7fb99e707292e4b6f59467bfbfddef4"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, [search]);
+  let searchHandler = (e) => {
+    setSearch(e.target.value);
   };
-
-  const handleNextButtonClick = () => {
-    if (selectedOption && selectedOption.isCorrect) {
-      setScore(score + 1);
+  const submitSearch = () => {
+    if (search !== "") {
+      fetch(
+        `https://emoji-api.com/emojis?search=${search}&access_key=67aa650ef7fb99e707292e4b6f59467bfbfddef4`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          if (res) {
+            setData(res);
+          } else {
+            setData([]);
+          }
+        });
     }
-
-    setSelectedOption(null);
-
-    if (currentQuestion === questions.length - 1) {
-      setShowScore(true);
-    } else {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-  };
-  const handlePrevButtonClick = () => {
-    if (currentQuestion === 0) {
-      return; // do nothing if we're already at the first question
-    }
-
-    setCurrentQuestion(currentQuestion - 1);
-  };
-
-  const handleRestartButtonClick = () => {
-    setCurrentQuestion(0);
-    setScore(0);
-    setShowScore(false);
-    setSelectedOption(null);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: "lightGrey",
-        minHeight: "100vh",
-        position: "absolute",
-      }}
-    >
-      <div className="box">
-        {showScore ? (
-          <div className="score-section">
-            <h1>
-              Your Score: {score} / {questions.length}
-            </h1>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleRestartButtonClick}
-            >
-              Restart Quiz
-            </Button>
+    <div className="App">
+      <div className="menu">
+        <div className="menu_item">
+          <h1> Emoji Search</h1>
+          <p>A simple emoji search with React</p>
+          <div>
+            <input
+              placeholder="search"
+              type="text"
+              value={search}
+              onChange={(e) => searchHandler(e)}
+            />
+            <button className="search" onClick={() => submitSearch()}>
+              Search
+            </button>
           </div>
-        ) : (
-          <>
-            <div className="question-section">
-              <h1 style={{ color: "" }}>
-                Question {currentQuestion + 1} / {questions.length}
-              </h1>
-              <div className="questionArea">
-                {questions[currentQuestion].question}
-              </div>
-            </div>
-            <div className="options-section">
-              {questions[currentQuestion].options.map((option) => (
-                <div>
-                  <Button
-                    key={option.id}
-                    variant="contained"
-                    color={selectedOption === option ? "success" : "secondary"}
-                    onClick={() => handleOptionClick(option)}
-                    sx={{
-                      marginBottom: "10px",
-                      width: "300px",
-                      marginTop: "10px",
-                    }}
-                  >
-                    {option.text}
-                  </Button>
-                </div>
-              ))}
-            </div>
-            <Box sx={{ display: "flex", marginLeft: "3%" }}>
-              {currentQuestion !== 0 && (
-                <Button
-                  variant="contained"
-                  sx={{ marginRight: "0px" }}
-                  onClick={handlePrevButtonClick}
-                >
-                  <ArrowLeft />
-                  Previous Question
-                </Button>
-              )}
-              <div
-                className="next-button"
-                onClick={handleNextButtonClick}
-                disabled={!selectedOption}
-                style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginLeft: "20%",
-                }}
-              >
-                {currentQuestion === questions.length - 1 ? (
-                  <Button variant="contained" color="success">
-                    Finish Quiz!
-                  </Button>
-                ) : (
-                  <Button variant="contained">
-                    Next Question
-                    <ArrowRight />
-                  </Button>
-                )}
-              </div>
-            </Box>
-          </>
-        )}
+        </div>
+      </div>
+      <div className="container">
+        {data.map((e, i) => (
+          <div className="card" key={e.slug}>
+            <p className="emo">{e.character}</p>
+            <p className="name">{e.unicodeName}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+
 export default App;
